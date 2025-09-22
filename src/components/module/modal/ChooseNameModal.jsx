55d048eb9c6e845f586fdfba8126ac3cc8842d3b
Wiliam <ariafardhossein@gmail.com>
2025-09-22 13:01:09 +0330

    set category select-box.

--- src/components/module/modal/ChooseNameModal.jsx
+++ src/components/module/modal/ChooseNameModal.jsx
@@ -1,12 +1,23 @@
-import { Modal, Form, Button } from "antd";
+import { useState } from "react";
+import { Modal, Form, Button, Select } from "antd";
 import { Formik, Field, ErrorMessage } from "formik";
+import { toast } from "react-toastify";
+import { useMutation, useQueryClient } from "react-query";
+import { request } from "@services/apiService.js";
+import logger from "@utils/logger.js";
 
 const ChooseNameModal = ({
   isOpenChooseNameModal,
   setIsOpenChooseNameModal,
+  optionsCategories,
   setName,
   title,
 }) => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [categoryTitle, setCategoryTitle] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+  const [loadingCreateCategory, setLoadingCreateCategory] = useState(false);
+  const [isCreatingCategory, setIsCreatingCategory] = useState(false);
   const initialValues = {
     name: "",
   };
@@ -19,12 +30,67 @@ const ChooseNameModal = ({
     return errors;
   };
 
+  const processedOptions = (optionsCategories ?? []).map((option) => ({
+    ...option,
+    value: option.value,
+  }));
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (data) => request({ method: "POST", url: "/api/categories", data }),
+    {
+      onSuccess: (data) => {
+        toast.success(data.data.message);
+        queryClient.invalidateQueries("getCategories");
+
+        setCategoryTitle("");
+        setCategoryDescription("");
+        setIsCreatingCategory(false);
+      },
+      onError: (error) => {
+        logger.error(error);
+      },
+      onSettled: () => {
+        setLoadingCreateCategory(false);
+      },
+    },
+  );
+
+  const handleCreateCategory = () => {
+    if (!categoryTitle) {
+      toast.error("Title is required!");
+      return;
+    }
+
+    const newCategory = {
+      title: categoryTitle,
+      type: "Files",
+      description: categoryDescription,
+    };
+
+    mutation.mutate(newCategory);
+    setLoadingCreateCategory(true);
+  };
+
+  const handleCategoryChange = (value) => {
+    setSelectedCategory(value);
+  };
+
+  const handleModalClose = () => {
+    setSelectedCategory(null);
+    setIsOpenChooseNameModal(false);
+    setIsCreatingCategory(false);
+    setCategoryTitle("");
+    setCategoryDescription("");
+  };
+
   return (
     <Modal
       className="font-Quicksand"
       title={`Choose Name ${title}`}
       open={isOpenChooseNameModal}
-      onCancel={() => setIsOpenChooseNameModal(false)}
+      onCancel={handleModalClose}
       footer={null}
     >
       <Formik
@@ -55,6 +121,64 @@ const ChooseNameModal = ({
                 className="text-red-500 text-[0.80rem] font-medium"
               />
             </div>
+
+            <Select
+              className="customSelect w-full font-Quicksand font-medium placeholder:font-medium"
+              options={processedOptions}
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+              placeholder="Categories"
+              allowClear
+              showSearch
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+            />
+
+            {!isCreatingCategory && (
+              <Button
+                onClick={() => setIsCreatingCategory(true)}
+                className="w-full font-Quicksand font-bold !bg-blue-200 !p-5 !shadow !text-blue-500 !text-[0.90rem] !border-[2.5px] !border-blue-500"
+              >
+                Create Category
+              </Button>
+            )}
+
+            {isCreatingCategory && (
+              <div className="w-full flex flex-col gap-2">
+                <input
+                  className="border-2 border-gray-200 outline-none p-3 rounded-md placeholder:font-medium"
+                  placeholder="Title"
+                  value={categoryTitle}
+                  onChange={(e) => setCategoryTitle(e.target.value)}
+                  required
+                />
+                <input
+                  className="border-2 border-gray-200 outline-none p-3 rounded-md placeholder:font-medium"
+                  placeholder="Description"
+                  value={categoryDescription}
+                  onChange={(e) => setCategoryDescription(e.target.value)}
+                />
+                <div className="w-full flex flex-row justify-center items-center gap-2">
+                  <Button
+                    onClick={() => setIsCreatingCategory(false)}
+                    className="w-1/2 font-Quicksand font-bold !bg-red-200 !p-4 !shadow !text-red-500 !text-[0.90rem] !border-[2.5px] !border-red-500"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={handleCreateCategory}
+                    loading={loadingCreateCategory}
+                    htmlType="button"
+                    className="w-1/2 font-Quicksand font-bold !bg-blue-200 !p-4 !shadow !text-blue-500 !text-[0.90rem] !border-[2.5px] !border-blue-500"
+                  >
+                    Create
+                  </Button>
+                </div>
+              </div>
+            )}
 
             <Button
               htmlType="submit"
