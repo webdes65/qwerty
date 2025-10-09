23d4c543eadd808c3cea33612f8997fffd0d9f61
Wiliam <ariafardhossein@gmail.com>
2025-10-09 10:39:14 +0330

    added map page & detail.

---
+++ src/pages/main/Map.jsx
@@ -0,0 +1,9 @@
+import MapCard from "@module/card/map/MapCard.jsx";
+
+export default function Map() {
+  return (
+    <div className="min-h-screen bg-white text-dark-100 dark:bg-gray-100 dark:text-white p-4">
+      <MapCard />
+    </div>
+  );
+}
