5ba8d21ba1a85a78d84f3f401478b5e1f9d14143
Wiliam <ariafardhossein@gmail.com>
2025-11-16 15:35:40 +0330

    separate logic & ui & using @apply.

--- src/components/module/modal/createItemModal/ButtonSection.jsx
+++ src/components/module/modal/createItemModal/ButtonSection.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { useState } from "react";
 import { Select, Spin } from "antd";
 import { Field } from "formik";
-import { request } from "@services/apiService.js";
+import ButtonSectionHandler from "@module/container/main/create-form/modal-handlers/ButtonSectionHandler.js";
+import "@styles/formAndComponentStyles.css";
 
 const ButtonSection = ({
   values,
@@ -19,30 +19,15 @@ const ButtonSection = ({
 }) => {
   const { isLoadingRegisters, registersError } = registersStatus;
   const { isLoading, error } = deviceStatus;
-  const [dispalyTypeButton, setDispalyTypeButton] = useState("externallink");
+  const [displayTypeButton, setDisplayTypeButton] = useState("externallink");
   const [optionsForm, setOptionsForm] = useState([]);
 
-  const { data: dataForms } = useQuery(["GetFormData"], () =>
-    request({
-      method: "GET",
-      url: "/api/forms",
-    }),
-  );
+  const {dataForms} = ButtonSectionHandler({setOptionsForm});
 
   const processedOptions = optionsForm.map((option) => ({
     ...option,
     value: option.value,
   }));
-
-  useEffect(() => {
-    if (dataForms) {
-      const ids = dataForms.data.map((item) => ({
-        label: item.name,
-        value: item.uuid,
-      }));
-      setOptionsForm(ids);
-    }
-  }, [dataForms]);
 
   return (
     <>
@@ -58,16 +43,16 @@ const ButtonSection = ({
                   key={type}
                   className={`w-4/12 flex flex-row justify-center items-center gap-2 bg-blue-100 rounded-md p-2 cursor-pointer
                     ${
-                      dispalyTypeButton === type
+                      displayTypeButton === type
                         ? "border-2 border-blue-500 text-dark-100"
                         : "border border-transparent text-gray-100"
                     }`}
-                  onClick={() => setDispalyTypeButton(type)}
+                  onClick={() => setDisplayTypeButton(type)}
                 >
                   <Field
                     type="radio"
                     className="hidden"
-                    checked={dispalyTypeButton === type}
+                    checked={displayTypeButton === type}
                   />
                   {type.charAt(0).toUpperCase() +
                     type.slice(1).replace(/([A-Z])/g, " $1")}
@@ -80,19 +65,19 @@ const ButtonSection = ({
             type="text"
             name="titlebtn"
             placeholder="Button Title"
-            className="border-2  border-gray-200 dark:border-gray-600 py-[0.20rem] px-3 rounded-md w-full outline-none text-dark-100  dark:bg-dark-100 dark:text-white"
+            className="py-[0.20rem] w-full uploadInputStyle"
           />
 
-          {dispalyTypeButton === "externallink" && (
+          {displayTypeButton === "externallink" && (
             <Field
               type="text"
               name="path"
               placeholder="External link"
-              className="border-2  border-gray-200 dark:border-gray-600 p-2 py-[0.20rem] px-3 rounded-md w-full outline-none text-dark-100  dark:bg-dark-100 dark:text-white"
+              className="px-3 py-[0.20rem] w-full uploadInputStyle"
             />
           )}
 
-          {optionsForm && dataForms && dispalyTypeButton === "chooseform" && (
+          {optionsForm && dataForms && displayTypeButton === "chooseform" && (
             <>
               <Select
                 className="customSelect w-full font-Quicksand"
@@ -128,7 +113,7 @@ const ButtonSection = ({
             </>
           )}
 
-          {dispalyTypeButton === "selectdevice" &&
+          {displayTypeButton === "selectdevice" &&
             (isLoading ? (
               <div className="w-full h-auto flex flex-row justify-center items-center bg-blue-50 p-2 rounded-lg">
                 <Spin />
@@ -152,8 +137,8 @@ const ButtonSection = ({
             ))}
 
           {selectedDeviceId &&
-            dispalyTypeButton !== "externallink" &&
-            dispalyTypeButton !== "chooseform" && (
+            displayTypeButton !== "externallink" &&
+            displayTypeButton !== "chooseform" && (
               <div className="w-full">
                 {isLoadingRegisters ? (
                   <div className="w-full h-auto flex flex-row justify-center items-center bg-blue-50 p-3 rounded-lg">
@@ -186,7 +171,7 @@ const ButtonSection = ({
 
                 <div className="flex flex-col justify-center items-center gap-2 pt-2">
                   <input
-                    className="border-2  border-gray-200 dark:border-gray-600 py-[0.20rem] px-3 rounded-md w-full outline-none text-dark-100  dark:bg-dark-100 dark:text-white"
+                    className="px-3 py-[0.20rem] w-full uploadInputStyle"
                     type="text"
                     value={infoReqBtn.title}
                     placeholder="Title req"
@@ -199,7 +184,7 @@ const ButtonSection = ({
                   />
 
                   <input
-                    className="border-2  border-gray-200 dark:border-gray-600 py-[0.20rem] px-3 rounded-md w-full outline-none text-dark-100  dark:bg-dark-100 dark:text-white"
+                    className="px-3 py-[0.20rem] w-full uploadInputStyle"
                     placeholder="Value req"
                     type="text"
                     value={infoReqBtn.value}
