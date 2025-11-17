9fff6c72032ff3a232618f99c76865060cbac82b
Wiliam <ariafardhossein@gmail.com>
2025-11-17 12:08:23 +0330

    fix warning.

--- src/components/module/container/main/create-form/FormDisplayHandler.js
+++ src/components/module/container/main/create-form/FormDisplayHandler.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 import { request } from "@services/apiService.js";
 import logger from "@utils/logger.js";
