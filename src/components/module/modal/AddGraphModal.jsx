517f0e7425294cbf6c4ca26e45d786ebff927abc
Wiliam <ariafardhossein@gmail.com>
2025-11-18 12:40:36 +0330

    separate logic & ui & using @apply.

--- src/components/module/modal/AddGraphModal.jsx
+++ src/components/module/modal/AddGraphModal.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useState } from "react";
 import { Button, Modal, Select, Spin } from "antd";
 import { Formik, Form, Field } from "formik";
 import CustomField from "@module/CustomField";
-import { request } from "@services/apiService.js";
-import logger from "@utils/logger.js";
+import AddGraphHandlers from "@module/container/main/graphs/AddGraphHandlers.js";
+import "@styles/formAndComponentStyles.css;";
 
 const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
-  const [submitPending, setSubmitPending] = useState(false);
+  const [optionsDevices, setOptionsDevices] = useState([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
+  const [gridValues, setGridValues] = useState([]);
 
   const initialValues = {
     title: "",
@@ -17,148 +17,23 @@ const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
     rows: "",
   };
 
-  const [grid, setGrid] = useState([]);
-  const [gridValues, setGridValues] = useState([]);
-
-  const generateGrid = (rows, columns) => {
-    const maxRows = Math.min(parseInt(rows || 0, 10), 3);
-    const maxColumns = Math.min(parseInt(columns || 0, 10), 3);
-    const newGrid = Array(maxRows)
-      .fill(null)
-      .map(() => Array(maxColumns).fill(null));
-    Array(maxRows)
-      .fill(null)
-      .map(() => Array(maxColumns).fill(false));
-    setGrid(newGrid);
-    setGridValues(
-      new Array(maxRows)
-        .fill(null)
-        .map(() =>
-          new Array(maxColumns).fill({ device: null, register: [], count: 0 }),
-        ),
-    );
-  };
-
-  const [optionsDevices, setOptionsDevices] = useState([]);
-  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
-
   const {
-    data: devicesData,
-    // isLoading: isLoadingDevices,
-    // error: devicesError,
-  } = useQuery(["getDevices"], () =>
-    request({ method: "GET", url: "/api/devices" }),
-  );
-
-  useEffect(() => {
-    if (devicesData) {
-      const newOptions = devicesData.data.map((item) => ({
-        label: item.name,
-        value: item.uuid,
-      }));
-      setOptionsDevices(newOptions);
-    }
-  }, [devicesData]);
-
-  const [optionsRegisters, setOptionsRegisters] = useState([]);
-
-  const {
-    data: registersData,
-    isLoading: isLoadingRegisters,
-    // error: registersError,
-  } = useQuery(
-    ["getRegisters", selectedDeviceId],
-    () =>
-      request({
-        method: "GET",
-        url: `/api/registers?device_id=${selectedDeviceId}`,
-      }),
-    {
-      enabled: !!selectedDeviceId,
-    },
-  );
-
-  useEffect(() => {
-    if (registersData) {
-      const newOptions = registersData.data.map((item) => ({
-        label: item.title,
-        value: item.uuid,
-        title: `${item.title} (${item.uuid})`,
-      }));
-      setOptionsRegisters(newOptions);
-    }
-  }, [registersData]);
-
-  const handleDeviceChange = (rowIndex, colIndex, value) => {
-    setGridValues((prevValues) => {
-      const updatedGrid = [...prevValues];
-      if (!updatedGrid[rowIndex]) updatedGrid[rowIndex] = [];
-      updatedGrid[rowIndex][colIndex] = {
-        ...updatedGrid[rowIndex][colIndex],
-        device: value,
-      };
-      return updatedGrid;
-    });
-  };
-
-  const handleRegisterChange = (rowIndex, colIndex, value) => {
-    setGridValues((prevValues) => {
-      const updatedGrid = [...prevValues];
-      if (!updatedGrid[rowIndex]) updatedGrid[rowIndex] = [];
-      updatedGrid[rowIndex][colIndex] = {
-        ...updatedGrid[rowIndex][colIndex],
-        register: value,
-      };
-      return updatedGrid;
-    });
-  };
-
-  const handleCountChange = (rowIndex, colIndex, value) => {
-    setGridValues((prevValues) => {
-      const updatedGrid = [...prevValues];
-      if (!updatedGrid[rowIndex]) updatedGrid[rowIndex] = [];
-      updatedGrid[rowIndex][colIndex] = {
-        ...updatedGrid[rowIndex][colIndex],
-        count: value,
-      };
-      return updatedGrid;
-    });
-  };
-
-  const onSubmit = async (values) => {
-    const flatGridValues = gridValues.flat().map((item) => ({
-      device: item.device,
-      registers: item.register,
-      count: item.count,
-    }));
-
-    const updatedValues = {
-      ...values,
-      charts: flatGridValues,
-    };
-
-    setSubmitPending(true);
-    mutation.mutate(updatedValues);
-  };
-
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation(
-    (data) => request({ method: "POST", url: "/api/templates", data }),
-    {
-      onSuccess: (data) => {
-        toast.success(data.data.message);
-        setIsOpenAddGraphModal(false);
-        queryClient.invalidateQueries("fetchGraphs");
-      },
-      onError: (error) => {
-        logger.error(error);
-      },
-      onSettled: () => {
-        setSubmitPending(false);
-      },
-    },
-  );
+    optionsRegisters,
+    submitPending,
+    grid,
+    isLoadingRegisters,
+    generateGrid,
+    handleDeviceChange,
+    handleRegisterChange,
+    handleCountChange,
+    onSubmit,
+  } = AddGraphHandlers({
+    setOptionsDevices,
+    selectedDeviceId,
+    setIsOpenAddGraphModal,
+    gridValues,
+    setGridValues,
+  });
 
   return (
     <Modal
@@ -201,7 +76,7 @@ const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
                     handleChange(e);
                     generateGrid(values.rows, e.target.value);
                   }}
-                  className="p-2 border-2 border-gray-200 dark:border-gray-600 rounded-lg w-full outline-none bg-white text-dark-100  dark:bg-dark-100 dark:text-white"
+                  className="!p-2 bg-white w-full inputStyle"
                 />
                 <Field
                   id="rows"
@@ -215,7 +90,7 @@ const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
                     handleChange(e);
                     generateGrid(e.target.value, values.columns);
                   }}
-                  className="p-2 border-2 border-gray-200 dark:border-gray-600 rounded-lg w-full outline-none bg-white text-dark-100  dark:bg-dark-100 dark:text-white"
+                  className="!p-2 bg-white w-full inputStyle"
                 />
               </div>
               <div className="w-full mt-4 space-y-3">
@@ -316,7 +191,7 @@ const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
                                 e.target.value,
                               )
                             }
-                            className="w-full border-2 border-gray-200 dark:border-gray-600 bg-white text-dark-100  dark:bg-dark-100 dark:text-white outline-none py-2 px-3 rounded-md font-medium placeholder:text-gray-400 dark:placeholder:text-white focus:border-blue-400 focus:ring-1 focus:ring-blue-400"
+                            className="w-full bg-white border-2 py-2 px-3 font-medium inputStyle placeholder:text-gray-400 dark:placeholder:text-white focus:border-blue-400 focus:ring-1 focus:ring-blue-400"
                             min="0"
                           />
                         </div>
@@ -330,7 +205,7 @@ const AddGraphModal = ({ isOpenAddGraphModal, setIsOpenAddGraphModal }) => {
                   type="primary"
                   htmlType="submit"
                   loading={submitPending}
-                  className="w-1/2 font-Quicksand !bg-blue-200 dark:!bg-blue-300 !p-5 !shadow !text-blue-500 dark:!text-blue-600 font-bold !text-[0.90rem] !border-[2.5px] !border-blue-500 dark:!border-blue-600"
+                  className="w-1/2 dark:!bg-blue-300 dark:!text-blue-600 dark:!border-blue-600 buttonPrimaryStyle"
                 >
                   Submit
                 </Button>
